99b953eee9f1f3baedbe19f57266180d735eb654
takuma-ru <kanekotakuma@team-lab.com>
2025-05-14 13:31:55 +0900

    🔥 @katu/transpiler

--- packages/transpiler/src/traverse/types.ts
+++
@@ -1,4 +0,0 @@
-import type * as t from "@babel/types";
-
-export type SignalInfo = { name: string; setter: string; init: t.Expression };
-export type EventBinding = { selector: string; event: string; handler: string };
